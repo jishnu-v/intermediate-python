d25e15b0677be845b13f25fca7c6ab092a950f4e
jishnu-v <jishnu.vasanthakumaran@schrodinger.com>
2024-02-22 02:05:43 +0530

    class

---
+++ 01_geometry_app/rectangle.py
@@ -0,0 +1,30 @@
+#My logic
+#steps
+
+#genrate 2 random x,y
+#ask for x range
+#ask for y range
+
+#fuction to check if x is in range
+#run function twice
+#print result
+
+# Their Logic
+# WRITE DOWN ALL OBJECTS NEEDED
+# MAKE CLASSES FOR EACH OBJECT
+# CREATE METHODS FOR EACH CLASS
+# CALL METHODS
+
+# FOR CLASSES WE USE CAMELCASE
+class Point:
+    
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+#create object instance
+point1 =  Point(1,2)
+
+
+
